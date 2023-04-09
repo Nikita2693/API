db143d72304794a8bb73a7e503671a4c754c8f9a
Nikita2693 <belkinnik93@gmail.com>
2023-04-09 12:26:30 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+import org.example.FormDate;
+import org.example.Post;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.patronymic = "Иванович";
+        post.surname = "Иванов";
+        post.phone = "+7 (999)-999-99-99";
+        post.pasport = "4444 № 44444444";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
+    }
+}
---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String phone;
+    public String pasport;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
